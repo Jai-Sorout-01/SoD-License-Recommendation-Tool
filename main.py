aa3e7dda6b750c070277f5121ee889a7df275872
Jai-Sorout-01 <jaisorout.300@gmail.com>
2025-10-09 15:54:45 +0530

    main.py

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from io import BytesIO
 from PIL import Image
 
 # STREAMLIT PAGE CONFIG
-st.set_page_config(page_title="SAP License Recommendation Tool", page_icon= r"https://github.com/Jai-Sorout-01/SoD-License-Recommendation-Tool/blob/main/Victora%20Logo.png", layout="wide")
+st.set_page_config(page_title="SAP License Recommendation Tool", page_icon= r"https://raw.githubusercontent.com/Jai-Sorout-01/SoD-License-Recommendation-Tool/main/Victora%20Logo.png", layout="wide")
 
 # COMPANY LOGO
-logo_path = r"https://github.com/Jai-Sorout-01/SoD-License-Recommendation-Tool/blob/main/Victora%20Logo.png"
+logo_path = r"logo_path = "https://raw.githubusercontent.com/Jai-Sorout-01/SoD-License-Recommendation-Tool/main/Victora%20Logo.png"
+
 try:
     logo = Image.open(logo_path)
     st.image(logo, width=200)
@@ -209,4 +210,5 @@ if user_file and license_master_file:
 
 else:
     st.info("⬅️ Please upload both the **User–Tcode** file and the **License Master** file to continue.")
+
 
